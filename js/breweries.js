59346ca58556c792d75fc71b084a7a409b7beb13
LeandervanAarde <200211@virtualwindow.co.za>
2022-03-02 10:24:21 +0200

    For loop
    For loop has been added, splitting into objects and not appending all.

--- js/breweries.js
+++ js/breweries.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 
 
 $(".brewery-type").on("change" , () =>{
+
+
     
  console.log($(".brewery-type ").val());
 
@@ -17,33 +19,53 @@ $(".brewery-type").on("change" , () =>{
 
  $.getJSON(BrewType, function(result){
 
-    console.log(result[1].name);
-
-
-    const Display = `
-
-<div class="row results p-3">
-
-<div class="col-12 result p-4 mt-4 mb-4">
-
-    <h3 class="text-center">`+result[1].name+`</h3>
-    <h5 class="text-center brew-info mt-3">Address</h5>
-    <h5 class="text-center brew-info mt-3">Brew type</h5>
-    <h5 class="text-center brew-info mt-3">website</h5>
-    <h5 class="text-center brew-info mt-3">website</h5>
-    <h5 class="text-center brew-info mt-3">website</h5>
-    <h5 class="text-center brew-info mt-3">website</h5>
-
     
 
-</div>
+    // console.log(result[1].brewery_type);
+
+    for(var i =0; i < result.length; i++){
+        console.log(result[i]);
+        
+        if( j === result[i].brewery_type ){
+
+            places = [result[i]];
 
 
-`
 
-$(".results").empty();
+            // console.log(result[i].brewery_type);
 
-$(".results").append(Display);
+            let Display = `
+
+            <div class="row results p-3">
+            
+            <div class="col-12 result p-4 mt-4 mb-4">
+            
+                <h3 class="text-center">`+result[i].name+`</h3>
+                <h5 class="text-center brew-info mt-3">Address</h5>
+                <h5 class="text-center brew-info mt-3">`+result[i].brewery_type+`</h5>
+                <h5 class="text-center brew-info mt-3">website</h5>
+                <h5 class="text-center brew-info mt-3">website</h5>
+                <h5 class="text-center brew-info mt-3">website</h5>
+                <h5 class="text-center brew-info mt-3">website</h5>
+        
+            </div>
+            
+            
+            `;
+              
+ 
+            //     $(".results").empty();
+            
+            // $(".results").append(Display);
+
+
+
+        }
+
+    }
+
+
+ 
 
 
 
