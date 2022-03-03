b5012f668d8e196e93bd0a395515b3b3d74bbab9
LeandervanAarde <200211@virtualwindow.co.za>
2022-03-03 23:31:03 +0200

    Update Breweries.js
    Breweries search input now accepting Caps through .toLowerCase()

--- js/breweries.js
+++ js/breweries.js
@@ -52,6 +52,7 @@ $(document).ready(function(){
         $(".results").empty();
         const states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
         let StateValue = $(".state").val();  
+        
         const BrewState = 'https://api.openbrewerydb.org/breweries?by_state='+StateValue;
 
         $.getJSON(BrewState, function(result){
@@ -59,6 +60,8 @@ $(document).ready(function(){
 
 
             for(var i = 0; i < result.length; i++){
+
+                let StateValueLow = StateValue.toLowerCase();
 
                 var Display = `
         
@@ -72,7 +75,7 @@ $(document).ready(function(){
                     <h5 class="text-center brew-info mt-3">`+result[i].phone+`</h5>
                 `;
 
-                var match = result[i].state.toLowerCase().match(StateValue);
+                var match = result[i].state.toLowerCase().match(StateValueLow);
 
                 
                 if (match.length != 0 ) {
